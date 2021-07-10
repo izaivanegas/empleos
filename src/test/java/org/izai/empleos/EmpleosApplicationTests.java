5c94dba137203c90b0e7b4fc7b651e017545aa3b
Izai <izai.vanegas@gmail.com>
2021-07-10 00:12:08 -0500

    Primer versionamiento

---
+++ src/test/java/org/izai/empleos/EmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.izai.empleos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
